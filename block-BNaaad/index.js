562cb80a6914ec986a04f5f8548cbb332eb64232
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-04-23 22:28:44 +0530

    Block4

--- block-BNaaad/index.js
+++ block-BNaaad/index.js
@@ -1,9 +1,10 @@
 console.log('Welcome to Node.js');
 
-function numberSum(N) {
+
     var total = 0;
-      for(var i = 1; i <= N; i++){
+      for(var i = 1; i <= 10; i++){
         total += i;
       }
-      return total;
-  }
+
+
+  console.log(total);
