4d325f988f1abd518cc9ef869b2e168ab7c51e8a
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-04-24 17:01:09 +0530

    Assignment1|Core Node1

--- block-BNaaaj/server.js
+++ block-BNaaaj/server.js
@@ -2,6 +2,7 @@ const { parse } = require('path');
 var url = require('url');
 var adr = `https://airindia.com/fares/calculate?from=delhi&to=detroit`;
 var parsedUrl = url.parse(adr,true);
-console.log(parsedUrl.query.toString())
+console.log(parsedUrl.query)
 
 console.log(parsedUrl.pathname);
+console.log(parsedUrl.protocol);
