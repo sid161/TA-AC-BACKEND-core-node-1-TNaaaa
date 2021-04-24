32c5c08f6993f6144df5166c4791765fbc858659
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-04-24 15:21:28 +0530

    Block3

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,7 +1,10 @@
-var fs = require('fs')
-fs.readFile('./content.md', filepath, (err, file) => {
-  console.log(err, content.toString())
+var fs = require('fs');
+fs.readFile('./content.md', (err, content) => {
+  console.log(content.toString())
 })
+
+var result = fs.readFileSync('./content.md')
+    console.log(result)
 
 let buff1 = Buffer.alloc(10)
 console.log(buff1)
